554d10842efb4d2c7543235e0a65e227050390ab
sagnik@work <sagnikchatterjee607@gmail.com>
2024-02-21 20:50:00 +0530

    src: added the beginner of the parser

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	//read two tokens , so that currToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
