c920da9bb6f1babaf73a45156be2c44922c43e1b
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-08 01:10:23 +0530

    update changes

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,17 +6,67 @@ import (
 	"github.com/sagnikc395/monkey/token"
 )
 
+func TestNextToken1(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d]- tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
 func TestNextToken(t *testing.T) {
 
+	// input := `let five = 5;
+	// let ten = 10;
+
+	// let add = fn(x, y) {
+	//  x + y;
+	// };
+
+	// let result = add(five, ten);
+	// `
 	input := `let five = 5;
 	let ten = 10;
-   	
-	let add = fn(x, y) {
-     x + y;
+	   let add = fn(x, y) {
+		 x + y;
 	};
-   	
-	let result = add(five, ten);
-    `
+	   let result = add(five, ten);
+	   !-/*5;
+	   5 < 10 > 5;
+
+	   if (5 < 10) {
+		return true;
+	   } else {
+		return false;
+	   }
+
+	   10 == 10;
+	   10 != 9;
+	   `
+
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -73,22 +123,4 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 
-	input = `let five = 5;
-	let ten = 10;
-	   let add = fn(x, y) {
-		 x + y;
-	};
-	   let result = add(five, ten);
-	   !-/*5;
-	   5 < 10 > 5;
-
-	   if (5 < 10) {
-		return true;
-	   } else {
-		return false;
-	   }
-
-	   10 == 10;
-	   10 != 9;
-	   `
 }
