2e648991d793d43674f50a957e2b2cbdc30d7b0f
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-09 16:37:17 +0530

    added changes

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -37,35 +37,16 @@ func TestNextToken1(t *testing.T) {
 	}
 }
 
-func TestNextToken(t *testing.T) {
-
-	// input := `let five = 5;
-	// let ten = 10;
-
-	// let add = fn(x, y) {
-	//  x + y;
-	// };
-
-	// let result = add(five, ten);
-	// `
+func TestNextToken2(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
-	   let add = fn(x, y) {
-		 x + y;
+	
+	let add = fn(x,y) {
+		x + y;
 	};
-	   let result = add(five, ten);
-	   !-/*5;
-	   5 < 10 > 5;
 
-	   if (5 < 10) {
-		return true;
-	   } else {
-		return false;
-	   }
-
-	   10 == 10;
-	   10 != 9;
-	   `
+	let result = add(five,ten);
+	`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -109,15 +90,60 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
-
 	l := New(input)
-
 	for i, tt := range tests {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d]- tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestNextToken3(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+	   !-/*5;
+	   5 < 10 > 5;
+	`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+	}
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d]- tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
