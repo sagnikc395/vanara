06bd466232c80d664ed1966a1339412aaf3d8a6d
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-05-26 23:56:55 +0530

    reset and start again

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/user"
-
-	"github.com/sagnikc395/monkey/repl"
-)
+import "fmt"
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Hello %s ! welcome to the monkey programming language\n", user.Username)
-	fmt.Printf("feel free to type in commands \n")
-	repl.Start(os.Stdin, os.Stdout)
+	fmt.Println("donkey")
 }
