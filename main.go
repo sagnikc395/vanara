52b0c0707ed0573054426c71caf2cfb2e3085143
sagnik@work <sagnikchatterjee607@gmail.com>
2024-05-27 11:23:19 +0530

    src: added repl and main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/sagnikc395/donkey/repl"
+)
 
 func main() {
-	fmt.Println("donkey")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the donkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
