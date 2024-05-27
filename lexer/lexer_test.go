5fe7c558eb8ec1e8a353f4e224a65002f3a695f5
sagnik@work <sagnikchatterjee607@gmail.com>
2024-05-27 11:01:17 +0530

    src: add new expressions and keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -17,6 +17,14 @@ func TextNextToken(t *testing.T) {
 	};
 
 	let result = add(five,ten);
+	!-/*5;
+	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
 	`
 	tests := []struct {
 		expectedType    token.TokenType
