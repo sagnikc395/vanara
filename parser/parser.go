75ec04a994341799614dc1d1cbf7aa987ad918a9
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-03 16:43:55 +0530

    src: parsing let statements ; getting good errors

--- parser/parser.go
+++ parser/parser.go
@@ -1,20 +1,22 @@
 package parser
 
 import (
-	"github.com/sagnikc395/donkey/ast"
-	"github.com/sagnikc395/donkey/token"
+	"fmt"
 
+	"github.com/sagnikc395/donkey/ast"
 	"github.com/sagnikc395/donkey/lexer"
+	"github.com/sagnikc395/donkey/token"
 )
 
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	//read the 2 tokens so that curToken and peekToken are both set
 	p.nextToken()
@@ -51,4 +53,49 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
 
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	//TODO: we are going to skip the expressions until we encoutner a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,14 +9,15 @@ import (
 
 func TestLetStatement(t *testing.T) {
 	input := `
-	let x = 5;
-	let y = 10;
-	let foobar = 838383`
+	let x 5;
+	let  = 10;
+	let  838383`
 
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -62,4 +63,17 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
