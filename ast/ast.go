b3eab9468f3b57fe8fcecbd678fbb264440d10bf
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-06-03 21:39:05 +0530

    src: added string implementations

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,15 @@
 package ast
 
-import "github.com/sagnikc395/donkey/token"
+import (
+	"bytes"
+
+	"github.com/sagnikc395/donkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	//useful for debuggin
+	String() string
 }
 
 type Statement interface {
@@ -18,6 +24,14 @@ type Expression interface {
 
 type Program struct {
 	Statements []Statement
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 func (p *Program) TokenLiteral() string {
@@ -58,4 +72,50 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+//string methods for different types of statements
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
