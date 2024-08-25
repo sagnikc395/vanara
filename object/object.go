309362a177f9617134cd2848845544ea5b17bcf1
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 20:40:23 +0530

    src: added opbject repr

---
+++ object/object.go
@@ -0,0 +1,86 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	IntegerObj          = "Integer"
+	BooleanObj          = "Boolean"
+	NullObj             = "Null"
+	ReturnValueObj      = "ReturnValue"
+	ErrorObj            = "Error"
+	FunctionObj         = "Function"
+	StringObj           = "String"
+	BuiltinObj          = "Builtin"
+	ArraysObj           = "Array"
+	HashObj             = "Hash"
+	CompiledFunctionObj = "CompiledFunction"
+	ClosureObj          = "Closure"
+)
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType {
+	return IntegerObj
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BooleanObj }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NullObj
+}
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return ReturnValueObj }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+
