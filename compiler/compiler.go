b3ed4a36236900ac5879a0b32414b8edbb58ae4a
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 21:53:08 +0530

    src: added compiler defns

---
+++ compiler/compiler.go
@@ -0,0 +1,32 @@
+package compiler
+
+import (
+	"github.com/sagnikc395/vanara/code"
+	"github.com/sagnikc395/vanara/object"
+)
+
+type Compiler struct {
+	instructions        code.Instructions
+	constants           []object.Object
+	lastInstruction     EmittedInstruction
+	previousInstruction EmittedInstruction
+	symbolTable         *SymbolTable
+	scopes              []CompilationScope
+	scopeIndex          int
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+type EmittedInstruction struct {
+	Opcode   code.Opcode
+	Position int
+}
+
+type CompilationScope struct {
+	instructions        code.Instructions
+	lastInstruction     EmittedInstruction
+	previousInstruction EmittedInstruction
+}
