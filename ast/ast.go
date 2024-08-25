3e597bcad6ed3e8494caa9fb3bb49290dda609cb
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 12:41:12 +0530

    src: define string literal

--- ast/ast.go
+++ ast/ast.go
@@ -259,4 +259,12 @@ func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode()      {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
 
