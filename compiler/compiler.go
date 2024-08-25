622701fa22fa401d4e8f734678c0e1c1256db07e
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 23:21:39 +0530

    src: vm methods

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -34,5 +34,33 @@ type CompilationScope struct {
 func NewWithState(s *SymbolTable, constants []object.Object) *Compiler {
 	compiler := New()
 	compiler.symbolTable = s
+	compiler.constants = constants
+	return compiler
 }
 
+func New() *Compiler {
+	mainScope := CompilationScope{
+		instructions:        code.Instructions{},
+		lastInstruction:     EmittedInstruction{},
+		previousInstruction: EmittedInstruction{},
+	}
+
+	symbolTable := NewSymbolTable()
+
+	for i, v := range object.Builtins {
+		symbolTable.DefineBuiltin(i, v.Name)
+	}
+
+	return &Compiler{
+		constants:   []object.Object{},
+		symbolTable: symbolTable,
+		scopes:      []CompilationScope{mainScope},
+		scopeIndex:  0,
+	}
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.currentInstructions(),
+	}
+}
--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,12 @@
 package vm
 
-import "github.com/sagnikc395/vanara/object"
+import (
+	"fmt"
+
+	"github.com/sagnikc395/vanara/code"
+	"github.com/sagnikc395/vanara/compiler"
+	"github.com/sagnikc395/vanara/object"
+)
 
 const (
 	StackSize  = 2048
@@ -39,3 +45,53 @@ func New(bytecode *compiler.Bytecode) *VM {
 	}
 }
 
+func NewWithGlobalsStore(bytecode *compiler.Bytecode, s []object.Object) *VM {
+	vm := New(bytecode)
+	vm.globals = s
+	return vm
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) LastPoppedStackElem() object.Object {
+	return vm.stack[vm.sp]
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
+
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	return o
+}
+
+func (vm *VM) executeBinaryOperation(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	leftType := left.Type()
+	rightType := right.Type()
+
+	switch {
+	case leftType == object.IntegerObj && rightType == object.IntegerObj:
+		return vm.executeBinaryIntegerOperation(op, left, right)
+	case leftType == object.StringObj && rightType == object.StringObj:
+		return vm.executeBinaryStringOperation(op, left, right)
+	default:
+		return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+	}
+}
