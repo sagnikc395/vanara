831e0fb2b54a706aec7d5d5f5cdaae4db7be4467
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 12:39:37 +0530

    src: define the boolean type

--- ast/ast.go
+++ ast/ast.go
@@ -240,3 +240,12 @@ func (ce *CallExpression) String() string {
 
 	return out.String()
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
