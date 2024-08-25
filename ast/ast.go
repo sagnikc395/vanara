485f37f195c242f85799500f1c693ef48a751bd5
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 12:40:09 +0530

    src: added integer literal

--- ast/ast.go
+++ ast/ast.go
@@ -249,3 +249,14 @@ type Boolean struct {
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+
