59c0d54edfd630a17711e9cf4bdda7349ee1a6e0
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-08-25 12:30:10 +0530

    src: added the prefix and infix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -141,3 +141,50 @@ func (bs *BlockStatement) String() string {
 
 	return out.String()
 }
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(token.LPAREN)
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(token.RPAREN)
+
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (oe *InfixExpression) expressionNode() {}
+func (oe *InfixExpression) TokenLiteral() string {
+	return oe.Token.Literal
+}
+
+func (oe *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString(token.LPAREN)
+	out.WriteString(oe.Left.String())
+	out.WriteString(" " + oe.Operator + " ")
+	out.WriteString(oe.Right.String())
+	out.WriteString(token.RPAREN)
+
+	return out.String()
+}
+
+
