cee72a12f62318abbd2fb4067e7f3cb55722405c
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-06 14:39:59 +0530

    src: added nexttoken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go/token"
+
 type Lexer struct {
 	input        string
 	position     int  // curr posn in input
@@ -10,4 +12,44 @@ type Lexer struct {
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
+}
+
+//helper method to make the usage of these fields easier to understand
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// predict the next token
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token., l.ch)
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{
+		Type:    tokenType,
+		Literal: string(ch),
+	}
 }
