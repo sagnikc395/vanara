b09275980aa4e874fb72d1efcca2698c795a220a
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-27 01:18:33 +0530

    src: define the ast

---
+++ parser/parser.go
@@ -0,0 +1,34 @@
+//construct ast with our parser
+
+package parser
+
+import (
+	"github.com/sagnikc395/vanara/ast"
+	"github.com/sagnikc395/vanara/lexer"
+	"github.com/sagnikc395/vanara/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//read the tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
