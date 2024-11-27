e622ede90192ef77468adfc28a1dcfade4cdc662
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-27 08:57:24 +0530

    src: added stringer methods for printing AST nodes

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/sagnikc395/vanara/token"
 )
 
@@ -8,6 +10,8 @@ import (
 type Node interface {
 	//used for debugging and testing
 	TokenLiteral() string
+	//help to print AST nodes for debugging and to compare them with other AST nodes
+	String() string
 }
 
 // some are statements
@@ -65,4 +69,59 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
