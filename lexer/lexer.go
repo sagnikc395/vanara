63e9bc4d649165b50712050b9dafcae95d25b8f0
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-24 12:15:40 +0530

    feat: added lexer and constructor

---
+++ lexer/lexer.go
@@ -0,0 +1,13 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
