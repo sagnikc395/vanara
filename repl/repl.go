15ccee1b39f9c6eeb6659bb4ae03ca25ba0fcda4
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-27 09:37:07 +0530

    feat: added repl

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/sagnikc395/vanara/lexer"
+	"github.com/sagnikc395/vanara/token"
+)
+
+const VERSION = `0.1.0`
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf("%s", fmt.Sprintf("vanara %s >>>", VERSION))
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
