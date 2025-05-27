f69a59bb2461aaeea23a553edad6791649b36971
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-27 09:38:55 +0530

    release 0.1.0 of vanara

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/sagnikc395/vanara/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Welcome to the Vanara programming environment!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
