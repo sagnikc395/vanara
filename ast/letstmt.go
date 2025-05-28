aefb33ba390e69bf92468ac354dfb1594b3a3a89
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-28 10:41:07 +0530

    feat: define the ast for the let statement

---
+++ ast/letstmt.go
@@ -0,0 +1,24 @@
+package ast
+
+import "github.com/sagnikc395/vanara/token"
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
