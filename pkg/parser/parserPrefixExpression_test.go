636c639c1f287a0ca33bef804db10f495c9e44ea
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-06-21 18:25:06 +0530

    test: added generic test rules on top of which we can build upon

--- pkg/parser/parserPrefixExpression_test.go
+++ pkg/parser/parserPrefixExpression_test.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/sagnikc395/vanara/pkg/ast"
@@ -47,22 +46,4 @@ func TestParsingPrefixExpressions(t *testing.T) {
 			return
 		}
 	}
-}
-
-func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
-	integ, ok := il.(*ast.IntegerLiteral)
-	if !ok {
-		t.Errorf("il not *ast.IntegerLiteral. got=%T", il)
-		return false
-	}
-	if integ.Value != value {
-		t.Errorf("integ.Value not %d. got=%d", value, integ.Value)
-		return false
-	}
-	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
-		t.Errorf("integ.TokenLiteral not %d. got=%s", value, integ.TokenLiteral())
-		return false
-	}
-
-	return true
 }
