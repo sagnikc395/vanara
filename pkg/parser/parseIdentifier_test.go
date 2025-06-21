39dd57350be4b3c047aebd9e3fecf81169c5fc2f
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-06-21 18:14:47 +0530

    test: added test to test the identifier

---
+++ pkg/parser/parseIdentifier_test.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/sagnikc395/vanara/pkg/ast"
+)
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+		return false
+	}
+	return true
+
+}
